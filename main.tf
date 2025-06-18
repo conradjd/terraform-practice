516c789f59c9c39956df6a7508f3b4ee383ffbae
conradjd <conradjdcosta@gmail.com>
2025-06-18 17:50:01 +0100

    Added module for security group

--- main.tf
+++ main.tf
@@ -21,42 +21,23 @@ data "aws_vpc" "default"{
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog_security_group.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "blog_security_group" {
-  name        = "blog_security_group"
-  description = "Allow http and https in. Allow everything out"
-  vpc_id      = data.aws_vpc.default.id 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "blog_sg"
+
+  vpc_id. = data.aws_vpc.default.id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog_security_group.id
-  }
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog_security_group.id
-  }
-
-resource "aws_security_group_rule" "blog_out" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog_security_group.id
-  }
